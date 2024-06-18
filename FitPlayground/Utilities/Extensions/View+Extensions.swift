da22e97fe171446fc8caafce374860da4993fe75
Maksim Matusevich <imulir@gmail.com>
2024-06-18 13:14:15 -0700

    removed navBar, custom radius extension

---
+++ FitPlayground/Utilities/Extensions/View+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  View+Extensions.swift
+//  FitPlayground
+//
+//  Created by Maksim Matusevich on 6/18/24.
+//
+
+import SwiftUI
+import UIKit
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, for corners: [UIRectCorner]) -> some View {
+        let rect = UnevenRoundedRectangle(
+            topLeadingRadius: corners.contains(where: { $0 == .topLeft }) ? radius : 0,
+            bottomLeadingRadius: corners.contains(where: { $0 == .bottomLeft }) ? radius : 0,
+            bottomTrailingRadius: corners.contains(where: { $0 == .bottomRight }) ? radius : 0,
+            topTrailingRadius: corners.contains(where: { $0 == .topRight }) ? radius : 0,
+            style: .continuous
+        )
+        return clipShape(rect)
+    }
+}
--- FitPlayground/Views/Screens/HomeTab/HomeTabView.swift
+++ FitPlayground/Views/Screens/HomeTab/HomeTabView.swift
@@ -10,58 +10,55 @@ import SwiftUI
 struct HomeTabView: View {
     private let workoutsInfo: [Workout] = [
         .init(name: "Core Engager 🎯", duration: 27, muscleGroups: [.abs, .back, .chest], status: .completed),
-        .init(name: "Chair Rounds!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds1!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds2!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds3!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds4!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds5!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds6!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds7!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds8!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds9!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds10!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds11!", duration: 11, muscleGroups: [.legs, .back], status: .active),
+        .init(name: "Chair Rounds12!", duration: 11, muscleGroups: [.legs, .back], status: .active),
         .init(name: "Leg Day", duration: 39, muscleGroups: [.legs], status: .active)
     ]
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color.appPrimary.ignoresSafeArea()
-                
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 18) {
-                        Text("\(.today)")
-                            .font(.appTextHeader1)
-                            .foregroundStyle(Color.textPrimary)
-                            .padding(.horizontal, 16)
-                        
-                        LazyVStack(alignment: .leading, spacing: 10) {
-                            ForEach(workoutsInfo, id: \.self) { info in
-                                WorkoutCell(data: info)
-                            }
-                            
-                            AddWorkoutCell()
+        ZStack {
+            Color.orange.ignoresSafeArea()
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 18) {
+                    Text("\(.today)")
+                        .font(.appTextHeader1)
+                        .foregroundStyle(Color.textPrimary)
+                        .padding(.top, 24)
+                        .padding(.horizontal, 32)
+                    
+                    LazyVStack(alignment: .leading, spacing: 10) {
+                        ForEach(workoutsInfo, id: \.self) { info in
+                            WorkoutCell(data: info)
                         }
+                    
+                        AddWorkoutCell()
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.horizontal, 16)
+                    .padding(.bottom, 64)
                 }
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    HStack {
-                        AsyncImage(
-                            url: URL(string: "https://picsum.photos/200"),
-                            content: { image in image.image?.resizable() }
-                        )
-                        .frame(width: 44)
-                        .clipShape(RoundedRectangle(cornerRadius: 18))
-                        
-                        VStack(alignment: .leading) {
-                            Text("\(.greetingsMorning),")
-                                .font(.appTextCaption2)
-                                .foregroundStyle(Color.textSecondary)
-                            Text("Maksim")
-                                .font(.appTextHeader3)
-                                .foregroundStyle(Color.textPrimary)
-                        }
-                    }
-                    .frame(height: 44)
-                }
+                .background(Color.appPrimary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .cornerRadius(StyleManager.dialogRadius, for: [.topLeft, .topRight])
+                .offset(y: headerHeight)
             }
         }
     }
+}
+
+// MARK: - Constants
+fileprivate extension HomeTabView {
+    var headerHeight: CGFloat { 160.0 }
 }
 
 #Preview {
