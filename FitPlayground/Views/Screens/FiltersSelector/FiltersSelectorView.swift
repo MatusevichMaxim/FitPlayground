da1b18274bfc21bc4a8f1d945921cdcab1dbfdbf
Maksim Matusevich <imulir@gmail.com>
2024-08-01 19:26:52 -0700

    New localization. Updated filter models. Added foldable list

--- FitPlayground/Views/Screens/FiltersSelector/FiltersSelectorView.swift
+++ FitPlayground/Views/Screens/FiltersSelector/FiltersSelectorView.swift
@@ -18,22 +18,54 @@ struct FiltersSelectorView: View {
             
             List {
                 Section {
-                    ForEach(viewModel.filtersManager.muscleGroupFilterStateModels, id: \.filter) { model in
-                        FilterItemCell(resource: model.filter.icon, title: model.filter.localizedName)
+                    ForEach(viewModel.filtersManager.muscleGroupModels) { filter in
+                        if let subfilters = filter.subfilters {
+                            DisclosureGroup {
+                                ForEach(subfilters) { subfilter in
+                                    makeItemLabel(for: subfilter)
+                                }
+                            } label: {
+                                makeItemLabel(for: filter)
+                            }
+                        } else {
+                            makeItemLabel(for: filter)
+                        }
                     }
                 } header: {
                     Text(String.muscleGroup)
+                        .font(.ms_bold_17)
+                        .foregroundStyle(Color.textPrimary)
                 }
+                .headerProminence(.increased)
                 
                 Section {
-                    ForEach(viewModel.filtersManager.equipmentFilterStateModels, id: \.filter) { model in
-                        FilterItemCell(resource: model.filter.icon, title: model.filter.localizedName)
+                    ForEach(viewModel.filtersManager.equipmentModels) { filter in
+                        makeItemLabel(for: filter)
                     }
                 } header: {
                     Text(String.equipment)
+                        .font(.ms_bold_17)
+                        .foregroundStyle(Color.textPrimary)
                 }
+                .headerProminence(.increased)
             }
+            .tint(Color.textPrimary)
         }
+    }
+}
+
+extension FiltersSelectorView {
+    private func makeItemLabel(for filter: FilterModel) -> Label<Text, Image> {
+        Label(
+            title: {
+                Text(filter.name)
+                    .font(.ms_bold_17)
+                    .foregroundStyle(Color.textPrimary)
+            },
+            icon: {
+                Image(filter.icon ?? .bin_icon)
+            }
+        )
     }
 }
 
